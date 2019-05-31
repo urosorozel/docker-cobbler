5558962a2fdc9a8e69ec31aa9f53f8d3ffd9504f
Uros Orozel <uros.orozel@gmail.com>
2019-05-31 08:53:32 +0000

    Add update-config.sh

---
+++ cobbler/tree/usr/local/bin/update-config.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "Updating configuration files from environment variables"
+j2 --undefined /etc/cobbler/settings.j2 -o /etc/cobbler/settings
+j2 --undefined /etc/cobbler/dhcp.template.j2 -o /etc/cobbler/dhcp.template
+j2 --undefined /var/lib/cobbler/snippets/preseed_bionic_post_deploy.j2 -o /var/lib/cobbler/snippets/preseed_bionic_post_deploy
+j2 --undefined /var/lib/cobbler/snippets/preseed_xenial_post_deploy.j2 -o /var/lib/cobbler/snippets/preseed_xenial_post_deploy
